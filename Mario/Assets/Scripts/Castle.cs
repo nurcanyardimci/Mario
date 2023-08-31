b535e0505e0cc2389a49b4fcfcd29048d7f4366e
Nurcan Yardımcı <nurcann.yrdmc@gmail.com>
2023-08-31 13:16:57 +0300

    Mario

---
+++ Mario/Assets/Scripts/Castle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Castle : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Application.LoadLevel("GameComplete");
+
+        }
+    }
+    public void ExitGame(){
+        Application.Quit();
+    }
+    public void Restart(){
+        SceneManager.LoadScene("GameOver");
+    }
+}
